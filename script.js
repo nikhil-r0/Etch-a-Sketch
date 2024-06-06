326bf5c6fb1b8de08bd6707aafe88edf065f620a
Nikhil <nikhilrrvk@gmail.com>
2024-06-06 19:28:38 +0530

    Create function to adjust grid size

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const container = document.querySelector('#container');
-let box = document.createElement('div');
-box.className = 'flex';
 const size = 38*16;
-box.style.height = size+'px';
-box.style.width = size+'px';
-box.style.flexWrap = 'wrap';
-for(let i = 0; i < 16*16;i++){
-    let square = document.createElement('div');
-    square.style.border = 'solid black 1px';
-    square.style.padding = '18px';
-    box.appendChild(square);
-}
-container.appendChild(box);
+createGrid(16);
 
+function createGrid(length){
+    let box = document.createElement('div');
+    box.className = 'flex';
+    box.style.height = size+'px';
+    box.style.width = size+'px';
+    box.style.flexWrap = 'wrap';
+    let padding = (size/length-2)/2;
+    console.log(padding);
+    n = length*length;
+    for(let i = 0; i < n;i++){
+        let square = document.createElement('div');
+        square.style.border = 'solid black 1px';
+        square.style.padding = padding + 'px';
+        box.appendChild(square);
+        square.addEventListener('mouseenter',function (){
+            square.style.backgroundColor = 'red';
+        })
+    }
+    container.appendChild(box);
+}
