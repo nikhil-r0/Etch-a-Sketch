3b339552867f677213d2f603d0827aa76c2c4d74
Nikhil <nikhilrrvk@gmail.com>
2024-06-06 20:07:19 +0530

    Add button to change grid length

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const container = document.querySelector('#container');
 const size = 38*16;
-createGrid(16);
+container.appendChild(createGrid(16));
+const gridSize = document.querySelector('#gridSize');
+gridSize.addEventListener('click', function(){
+    length = parseInt(prompt('Enter number of rows or columns for the grid(1-100):'));
+    if (length <= 100 && length > 0){
+        const box = createGrid(length);
+        container.removeChild(container.lastChild);
+        container.appendChild(box);
+    }
+    else if(!Number.isNaN(length)){
+        alert('The given number must be an integer greater than 0 and less than 100!');
+        console.log(length,length);
+    }
+})
 
 function createGrid(length){
     let box = document.createElement('div');
@@ -20,5 +33,5 @@ function createGrid(length){
             square.style.backgroundColor = 'red';
         })
     }
-    container.appendChild(box);
+    return box;
 }
